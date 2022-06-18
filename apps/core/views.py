2de50adbfd21a979acfa7db1099259c81f1cb2ac
Vinnguyen <vinphunguyen.542@gmail.com>
2022-06-18 18:20:50 +0700

    day4

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,14 @@
 import re
 from django.shortcuts import render
 
+from apps.product.models import Product
+
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html')
+    stand_products = Product.objects.all() [0:3]
+    newest_products = Product.objects.all() [0:8]
+
+    return render(request, 'core/home.html', {'newest_products':newest_products, 'stand_products':stand_products})
 
 def contact(request):
     return render(request, 'core/contact.html')
